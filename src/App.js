1a72d5cb3d74a5be8b8e99703bcc834d3fcd7c67
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-07-24 10:24:01 +0800

    Added comment and change readme.md

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import { useState } from 'react';
 
 function App() {
 
+  //To save the state of inputText when there is a change, initial state set to blank ""
   const [inputText, setInputText] = useState("");
+  //To hold the value of an array of items, initial state is set to empty [] 
   const [items, setItems] = useState([]);
+
   function handleChange(event) {
+    //Hold the value of the inputText from user
     setInputText(event.target.value);
   }
 
@@ -30,7 +34,7 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map((todoItem) => (
+          {items.map((todoItem) => ( //mapping through each of the item and return a list of items being added
             <li>{todoItem}</li>
           ))}
         </ul>
